3bdd50aa44cf1e02e818e0aaa9234d821f3d4cd6
Mohammad Abir Reza <abir.cse.cou@gmail.com>
2018-03-09 21:23:46 +0600

    factory class & factory method

---
+++ factory class & factory method using beans/Address.java
@@ -0,0 +1,35 @@
+package com.abir.Hospital;
+
+public class Address {
+
+	private String street;
+	private String postCode;
+	
+	public Address() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Address(String street, String postCode) {
+	
+		this.street = street;
+		this.postCode = postCode;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getPostCode() {
+		return postCode;
+	}
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+	@Override
+	public String toString() {
+		return "Address [street=" + street + ", postCode=" + postCode + "]";
+	}
+	
+	
+}
---
+++ factory class & factory method using beans/Patient.java
@@ -0,0 +1,72 @@
+package com.abir.Hospital;
+
+public class Patient {
+
+	private int id;
+	private String name ;
+	private int nationalId;
+	
+	private Address address;
+	
+	 public Patient() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Patient(int id, String name) {
+		
+		this.id = id;
+		this.name = name;
+	}
+	
+	
+	public void onCreate() {
+		System.out.println("patient created " +this);
+	};
+	
+	public void onDestroy() {
+		System.out.println("destroyed patient");
+	}
+	
+	public static Patient getInstance(int id,String name) {
+		System.out.println("this is factory method");
+		return new Patient (id,name);
+	}
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNationalId() {
+		return nationalId;
+	}
+
+	public void setNationalId(int nationalId) {
+		this.nationalId = nationalId;
+	}
+
+	@Override
+	public String toString() {
+		return "Patient [id=" + id + ", name=" + name + ", nationalId=" + nationalId + ", address=" + address + "]";
+	}
+
+	
+	
+	
+	
+}
